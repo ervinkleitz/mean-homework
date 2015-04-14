bfce406026a1b251840b502a7a28ef1a124e939b
Ervin Gonzales <ervinkleitz@gmail.com>
2015-04-14 13:02:01 -0700

    Finished array

--- week2/battleship.js
+++ week2/battleship.js
@@ -1,13 +1,9 @@
 var matrix = [];
-
-for ( i = 0; i <= 10; i++ ) {
-
-	for (var j = 0; j <= 10 ; j++) {
-		
-		matrix.push([i,j]);
-		
+for ( i = 0; i < 5; i++ ) {
+	var line = [];
+	for ( var j = 0; j < 5; j++ ) {
+		line.push([i,j]);
 	}
-
+	matrix.push(line);
 }
-
-console.log(matrix);
+console.log(matrix[0]);
