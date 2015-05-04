af5548cf3719be040710ec735e910387f51ddb20
Ervin Gonzales <ervinkleitz@gmail.com>
2015-05-04 12:39:15 -0700

    Redid functions

--- week4/script.js
+++ week4/script.js
@@ -1,40 +1,30 @@
 $(document).ready( function(){
-    
-	getData();
 	
-    //***** Global var declarations *****//
+	$.ajax('http://mean.codingcamp.us:8888/ervinkleitz/longboards',{
+	method: 'GET'
+	})
+	.done( getLongboardsData )
+	.fail(function(){
+	console.log('GET Failed');
+	}); 
+	
+	//***** Global var declarations *****//
     var brand, name, length, style, price, bottomImgUrl, topImgUrl, masterList, editList;
 	var mainItemList = '';
 	var formCounter = 0;
-	var formList = document.getElementById( 'form' ).innerHTML;
+	var formList = document.getElementById( 'add-form' ).innerHTML; 
 	
 	//Hides the ' + ' button, table, and preview button
-	document.getElementById( 'addItem' ).style.visibility = 'hidden';
+	document.getElementById( 'addItem-button' ).style.visibility = 'hidden';
 	document.getElementById( 'form' ).style.visibility = 'hidden';
-	document.getElementById( 'preview' ).style.visibility = 'hidden';
+	document.getElementById( 'preview-button' ).style.visibility = 'hidden'; 
 	
 	//***** Event listeners *****//
-	document.getElementById( 'edit' ).addEventListener( 'click', editItems );
-	document.getElementById( 'addItem' ).addEventListener( 'click', addForm );
+	document.getElementById( 'edit-button' ).addEventListener( 'click', editItems );
+	document.getElementById( 'addItem-button' ).addEventListener( 'click', addForm ); 
+	document.getElementById( 'preview-button' ).addEventListener( 'click', function reload() { location.reload;} );
 	
-	////////////////////////////////////////////////////////////
-	///////////////////***** Functions *****////////////////////
-	////////////////////////////////////////////////////////////
-	
-	//***** Function to retrieve data from the server
-	function getData(){
-	
-		$.ajax('http://mean.codingcamp.us:8888/ervinkleitz/longboards',{
-		method: 'GET'
-		})
-		.done( getLongboardsData )
-		.fail(function(){
-		  console.log('GET Failed');
-		});
-	}//End of getData
-	
-    //***** Displays the saved longboards for sale *****//
-    function getLongboardsData( data ){
+	function getLongboardsData( data ){
         masterList = data;
 		var itemList = '';
         for ( var index = 0; index < masterList.length; index++ ) {
@@ -55,17 +45,30 @@ $(document).ready( function(){
 		}
 		
 		document.getElementById( 'items' ).innerHTML = mainItemList;
-    }//End of getLongboardsData
+    }//End of getLongboardsData 
 	
 	//***** Displays the edit page *****//
 	function editItems(){
 		editList = '';
 		document.getElementById( 'form' ).style.visibility = 'visible';
-		document.getElementById( 'addItem' ).style.visibility = 'visible';
-		document.getElementById( 'preview' ).style.visibility = 'visible';
-		document.getElementById( 'edit' ).style.visibility = 'hidden';
+		document.getElementById( 'addItem-button' ).style.visibility = 'visible';
+		document.getElementById( 'preview-button' ).style.visibility = 'visible';
+		document.getElementById( 'edit-button' ).style.visibility = 'hidden';
 		document.getElementById( 'items' ).style.display = 'none';
 		
+		editList += '<table class="col-md-3">';
+		editList += '<tr><td>ID:</td><td class="text-right"><input type="text" id="item-id" value=""></td>';
+		editList += '<tr><td>Brand:</td><td class="text-right" id="test-id"><input type="text" id="brand-id"';
+		editList += 'value=""></td></tr><tr><td>Name:</td><td class="text-right"><input type="text" id="name-id"';
+		editList += 'value=""></td></tr><tr><td>Length:</td><td class="text-right"><input type="number" id="length-id"';
+		editList += 'value="">"</td></tr><tr><td>Style:</td><td class="text-right"><input type="text" id="style-id"';
+		editList += 'value=""></td></tr><tr><td>Price:</td><td class="text-right">$<input type="number" id="price-id"';
+		editList += 'value=""</td></tr><tr><td>Bottom Image:</td><td class="text-right"><input type="text" placeholder="(url)" id="bottom-url"';
+		editList += 'value=""></td></tr><tr><td>Top Image:</td><td class="text-right"><input type="text" placeholder="(url)" id="top-url"';
+		editList += 'value=""></td></tr>';
+		editList += '</tr><tr><td><a class="delete" id="delete-item" href="#">delete</a></td><td class="text-right"><button class="save" id="update">update</button></td></tr></table><hr>';
+		editList += '<br>Longboards Currently on Sale<hr>';
+		//Display list of Longboards that are currently on sale
 		for ( var itemsIndex = 0; itemsIndex < masterList.length; itemsIndex++ ) {
 			var editBrand = masterList[ itemsIndex ][ 'brand' ];
 			var editName = masterList[ itemsIndex ][ 'name' ];
@@ -76,41 +79,29 @@ $(document).ready( function(){
 			var editTopUrl = masterList[ itemsIndex ][ 'top_img_url' ];
 			var itemId = masterList[ itemsIndex ][ '_id' ];
 			
-			editList += '<table class="col-md-3">';
-			//Put in ID in such a way that user can't change/modify it
-			editList += '<tr><td>ID:</td><td class="text-right" id="item-id">' + itemId + '</td>';
-			editList += '<tr><td>Brand:</td><td class="text-right" id="test-id"><input type="text" id="brand-id"';
-			editList += 'value="' + editBrand + '"></td></tr><tr><td>Name:</td><td class="text-right"><input type="text" id="name-id"';
-			editList += 'value="' + editName + '"></td></tr><tr><td>Length:</td><td class="text-right"><input type="number" id="length-id"';
-			editList += 'value="' + editLength +'">"</td></tr><tr><td>Style:</td><td class="text-right"><input type="text" id="style-id"';
-			editList += 'value="' + editStyle + '"></td></tr><tr><td>Price:</td><td class="text-right">$<input type="number" id="price-id"';
-			editList += 'value="' + editPrice + '"</td></tr><tr><td>Bottom Image:</td><td class="text-right"><input type="text" placeholder="(url)" id="bottom-url"';
-			editList += 'value="' + editBottomUrl + '"></td></tr><tr><td>Top Image:</td><td class="text-right"><input type="text" placeholder="(url)" id="top-url"';
-			editList += 'value="' + editTopUrl + '"></td></tr><tr><td><a class="delete" id="delete" href="#">delete</a></td><td class="text-right"><button class="save" id="update">update</button></td></tr></table>';
-		}
+			editList += '<table class="col-md-4">';
+			editList += '<tr><td>ID:</td><td>' +itemId+ '</td>';
+			editList += '<tr><td>Brand:</td><td>' + editBrand + '</td></tr>';
+			editList += '<tr><td>Name:</td><td>' + editName + '</td></tr>';
+			editList += '<tr><td>Length:</td><td>' + editLength + '</td></tr>';
+			editList += '<tr><td>Style:</td><td>' + editStyle + '</td></tr>';
+			editList += '<tr><td>Price:</td><td>' + editPrice + '</td></tr>';
+			editList += '<tr><td>Bottom Url:</td><td>' + editBottomUrl + '</td></tr>';
+			editList += '<tr><td>Top Url:</td><td>' + editTopUrl + '</td></tr>';
+			editList += '<tr><td></td></tr></table><hr>';
+		}//End of for loop
 		
 		document.getElementById( 'form' ).innerHTML = editList;
 		//***** Event Listeners when editing *****//
 		document.getElementById( 'update' ).addEventListener( 'click', updateItem );
-		document.getElementById( 'preview' ).addEventListener( 'click', getData );
-//		document.getElementById( 'delete' ).addEventListener( 'click', deleteItem );
+//		document.getElementById( 'delete-item' ).addEventListener( 'click', deleteItem );
 		
-	}//end of editItems
+	}//end of editItems 
 	
-	//***** Displays one more form *****//
-	function addForm(){
-		document.getElementById('form').style.display = 'none';
-		var tmpList = formList;
-		for ( var numberOfForms = 1; numberOfForms <= formCounter; numberOfForms++ ) {
-			tmpList += formList;
-		}
-		document.getElementById( 'add-form' ).innerHTML = tmpList;
-		formCounter++;
-		document.getElementById( 'save' ).addEventListener( 'click', save );
-	}//end of addForm
-
-	//***** Saves data (sends to server) *****//
-	function save(){
+	//***** Function that updates Longboards. Shows a list of available longboards *****//
+	//***** and a form to upate the list *****//
+	function updateItem(){
+		
 		var getBrand = document.getElementById( 'brand-id' ).value;
 		var getName = document.getElementById( 'name-id' ).value;
 		var getLength = document.getElementById( 'length-id' ).value;
@@ -118,6 +109,51 @@ $(document).ready( function(){
 		var getPrice = document.getElementById( 'length-id' ).value; 
 		var getBottomUrl = document.getElementById( 'bottom-url' ).value;
 		var getTopUrl = document.getElementById( 'top-url' ).value;
+		var getId = document.getElementById( 'item-id' ).value;
+		
+		var dataToPut = {
+			brand: getBrand,
+			name: getName,
+			length: getLength,
+			style: getStyle,
+			price: getPrice,
+			bottom_img_url: getBottomUrl,
+			top_img_url: getTopUrl
+		};
+
+		$.ajax({
+			method: 'PUT',
+			url: 'http://mean.codingcamp.us:8888/ervinkleitz/longboard/' + getId,
+			data: JSON.stringify(dataToPut), 
+			success: function(data) { alert( 'Item Updated!' ); console.log(JSON.stringify(dataToPut)); },
+			contentType: 'application/x-www-form-urlencoded',
+			dataType: 'json'
+		});
+		
+	}//End of updateItem
+	
+	//***** Displays one more form *****// 
+	function addForm(){
+		document.getElementById('form').style.display = 'none';
+		document.getElementById('add-form').style.visibility = 'visible';
+		var tmpList = formList;
+		for ( var numberOfForms = 1; numberOfForms <= formCounter; numberOfForms++ ) {
+			tmpList += formList;
+		}
+		document.getElementById( 'add-form' ).innerHTML = tmpList;
+		formCounter++;
+		document.getElementById( 'save-added' ).addEventListener( 'click', save );
+	}//end of addForm
+
+	//***** Saves data (sends to server) *****//
+	function save(){
+		var getBrand = document.getElementById( 'add-brand' ).value;
+		var getName = document.getElementById( 'add-name' ).value;
+		var getLength = document.getElementById( 'add-length' ).value;
+		var getStyle = document.getElementById( 'add-style' ).value;
+		var getPrice = document.getElementById( 'add-price' ).value; 
+		var getBottomUrl = document.getElementById( 'add-bottom-url' ).value;
+		var getTopUrl = document.getElementById( 'add-top-url' ).value;
 		
 		var dataToPost = {
 			brand: getBrand,
@@ -136,42 +172,7 @@ $(document).ready( function(){
 			success: function(data) { alert( 'Item Saved!' ); console.log(JSON.stringify(dataToPost)); }
 		});
 
-	}//End of save
+	}//End of save 
 	
-	//***** Function to update Longboard *****//
-	function updateItem(){
-		
-		var getBrand = document.getElementById( 'brand-id' ).value;
-		var getName = document.getElementById( 'name-id' ).value;
-		var getLength = document.getElementById( 'length-id' ).value;
-		var getStyle = document.getElementById( 'style-id' ).value;
-		var getPrice = document.getElementById( 'length-id' ).value; 
-		var getBottomUrl = document.getElementById( 'bottom-url' ).value;
-		var getTopUrl = document.getElementById( 'top-url' ).value;
-		var getId = document.getElementById( 'item-id' ).innerHTML;
-		
-		var dataToPut = {
-			brand: getBrand,
-			name: getName,
-			length: getLength,
-			style: getStyle,
-			price: getPrice,
-			bottom_img_url: getBottomUrl,
-			top_img_url: getTopUrl
-		};
-		
-		$.ajax({
-			method: 'PUT',
-			url: 'http://mean.codingcamp.us:8888/ervinkleitz/longboard/' + getId,
-			data: JSON.stringify(dataToPut), 
-			success: function(data) { alert( 'Item Updated!' ); console.log(JSON.stringify(dataToPut)); }
-//			,
-//			contentType: 'application/x-www-form-urlencoded',
-//			dataType: 'json'
-		});
-		
-	}
 	
-	//***** Function to delete Longboard record
-    
 });
